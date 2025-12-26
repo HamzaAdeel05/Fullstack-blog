bd70a618a287637fa8c7af3822d3627dec64be03
HamzaAdeel05 <hamza.adeel.2005@gmail.com>
2025-12-26 10:35:44 +0500

    posting

--- lib/models/Blog.ts
+++ lib/models/Blog.ts
@@ -6,9 +6,9 @@ const BlogSchema = new mongoose.Schema(
         title: { type: String, required: true },
         category: { type: String, required: true },
         blogDes: { type: String, required: true },
-        avatar: { type: String, required: true },
+        avatar: { type: String, },
         description: { type: String, required: true },
-        image: { type: String, required: true },
+        image: { type: String,  },
 
     },{
         timestamps:true,
